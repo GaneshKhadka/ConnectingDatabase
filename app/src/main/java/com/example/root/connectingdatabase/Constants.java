fbd62dd21c8d7fa0f721c5f0789b43271d194a62
root <root@localhost.localdomain>
2018-01-12 05:20:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/root/connectingdatabase/Constants.java
@@ -0,0 +1,10 @@
+package com.example.root.connectingdatabase;
+
+/**
+ * Created by root on 1/12/18.
+ */
+
+public class Constants {
+    private static final String ROOT_URL = "http://192.168.0.105/android/v1/";
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+}
